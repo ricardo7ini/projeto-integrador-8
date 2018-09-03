55e8dfd0ae5eac85e2d88a526cb5f8a09072728a
jose-wdg <contact@wedrivegrowth.com>
2018-09-03 17:30:52 -0300

    daly 3

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//toggle menu <===
+$(document).ready(function(){
+    $(".icons").click(function(){
+        $(".menu-item-mobil").toggle();
+    });
+});
+//login
+$(document).ready(function(){
+    $(".icon-login").click(function(){
+        $(".menu-item-mobil").toggle();
+    });
+});
