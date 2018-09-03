2937cfe0f76795bf6fc471023f3ffbf086a49393
unknown <andres@digitalhouse.com>
2018-09-03 19:28:55 -0300

    include nav login.html

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 //toggle menu <===
 $(document).ready(function(){
-    $(".icons").click(function(){
+    $(".icons").click(function(){//quando clicar no icone executa
         $(".menu-item-mobil").toggle();
     });
 });
 //login
 $(document).ready(function(){
     $(".icon-login").click(function(){
-        $(".menu-item-mobil").toggle();
+        $("").toggle();
     });
 });
